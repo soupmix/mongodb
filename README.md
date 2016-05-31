14e96c7b93283e904c9189ea6b31b7e6651e71dc
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2016-05-31 11:26:26 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,116 @@
+# Soupmix
+
+
+[![Latest Stable Version](https://poser.pugx.org/soupmix/mongodb/v/stable)](https://packagist.org/packages/soupmix/mongodb) [![Total Downloads](https://poser.pugx.org/soupmix/mongodb/downloads)](https://packagist.org/packages/soupmix/mongodb) [![Latest Unstable Version](https://poser.pugx.org/soupmix/mongodb/v/unstable)](https://packagist.org/packages/soupmix/mongodb) [![License](https://poser.pugx.org/soupmix/mongodb/license)](https://packagist.org/packages/soupmix/mongodb)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/soupmix/mongodb/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/soupmix/mongodb/)
+
+Simple low level  MongoDB adapter to handle CRUD operations written in PHP. This library does not provide any ORM or ODM. 
+
+
+## Installation
+
+It's recommended that you use [Composer](https://getcomposer.org/) to install Soupmix.
+
+```bash
+$ composer require soupmix/mongodb "~0.1"
+```
+
+This will install Soupmix and all required dependencies. Soupmix requires PHP 5.4.0 or newer, mongodb extension: 1.1.0 or newer, [mongo-php-library](https://github.com/mongodb/mongo-php-library) library  or newer form MongoDB.
+
+## Documentation
+
+[API Documentation](https://github.com/soupmix/base/blob/master/docs/API_Documentation.md): See details about the db adapters functions:
+
+## Usage
+```
+// Connect to MongoDB Service
+$adapter_config = [];
+$adapter_config['db_name'] ='db_name';
+$adapter_config['connection_string']="mongodb://127.0.0.1";
+$adapter_config['options'] =[];
+$m=new Soupmix\Adapters\MongoDB($adapter_config);
+
+
+$docs = [];
+$docs[] = [
+    "full_name" => "John Doe",
+      "age" => 33,
+      "email"    => "johndoe@domain.com",
+      "siblings"=> [
+        "male"=> [
+          "count"=> 1,
+          "names"=> ["Jack"]
+        ],
+        "female"=> [
+          "count" => 1,
+          "names" =>["Jane"]
+        ]      
+      ]
+];
+$docs[] = [
+    "full_name" => "Jack Doe",
+      "age" => 38,
+      "email"    => "jackdoe@domain.com",
+      "siblings"=> [
+        "male"=> [
+          "count"=> 1,
+          "names"=> ["John"]
+        ],
+        "female"=> [
+          "count" => 1,
+          "names" =>["Jane"]
+        ]      
+      ]
+];
+
+$docs[] = [
+    "full_name" => "Jane Doe",
+      "age" => 29,
+      "email"    => "janedoe@domain.com",
+      "siblings"=> [
+        "male"=> [
+          "count"=> 2,
+          "names"=> ["Jack","John"]
+        ],
+        "female"=> [
+          "count" => 0,
+          "names" =>[]
+        ]      
+      ]
+];
+
+foreach($docs as $doc){
+    // insert user into database
+    $mongo_user_id = $m->insert("users",$doc);
+}
+// get user data using id
+$user_data = $m->get('users', $mongo_user_id);
+
+
+$filter = ['age_gte'=>0];
+// update users' data that has criteria encoded in $filter
+$set = ['is_active'=>1,'is_deleted'=>0];
+
+$i = $m->update("users", $filter, $set);
+
+$filter = ["siblings.male.count__gte"=>2];
+
+//delete users that has criteria encoded in $filter
+$m->delete('users', $filter);
+
+
+
+// user's age lower_than_and_equal to 34 or greater_than_and_equal 36 but not 38
+$filter = [['age__lte'=>34,'age__gte'=>36],"age__not"=>38];
+
+//find users that has criteria encoded in $filter
+$docs = $m->find("users", $filter);
+
+
+```
+
+
+
+## Contribute
+* Open issue if found bugs or sent pull request.
+* Feel free to ask if you have any questions.
---
+++ src/MongoDB.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace Soupmix;
+/*
+MongoDB Adapter
+*/
+Use MongoDB as MongoDBLib;
+
+class MongoDB implements Base
+{
+    public $conn = null;
+
+    private $dbName = null;
+
+    public $db = null;
+
+    public function __construct($config)
+    {
+        $this->dbName = $config['db_name'];
+        $this->connect($config);
+    }
+
+    public function connect($config)
+    {
+        $this->conn = new MongoDBLib\Client($config['connection_string'], $config['options']);
+        $this->db = $this->conn->{$this->dbName};
+    }
+
+    public function create($collection, $config)
+    {
+        return $this->db->createCollection($collection);
+    }
+
+    public function drop($collection, $config)
+    {
+        return $this->db->dropCollection($collection);
+    }
+
+    public function truncate($collection, $config)
+    {
+        $this->db->dropCollection($collection);
+
+        return $this->db->createCollection($collection);
+    }
+
+    public function createIndexes($collection, $indexes)
+    {
+        $collection = $this->db->selectCollection($collection);
+
+        return $collection->createIndexes($indexes);
+    }
+
+    public function insert($collection, $values)
+    {
+        $collection = $this->db->selectCollection($collection);
+        $result = $collection->insertOne($values);
+        $docId = $result->getInsertedId();
+        if (is_object($docId)) {
+            return (string) $docId;
+        }
+        return null;
+        
+    }
+
+    public function get($collection, $docId)
+    {
+        $collection = $this->db->selectCollection($collection);
+        $filter = ['_id' => new \MongoDB\BSON\ObjectID($docId)];
+        $options = [
+            'typeMap' => ['root' => 'array', 'document' => 'array'],
+        ];
+        $result = $collection->findOne($filter, $options);
+        if ($result!==null) {
+            $result['id'] = (string) $result['_id'];
+            unset($result['_id']);
+        }
+
+        return $result;
+    }
+
+    public function update($collection, $filter, $values)
+    {
+        $collection = $this->db->selectCollection($collection);
+        $filter = self::buildFilter($filter)[0];
+        $values_set = ['$set' => $values];
+        if (isset($filter['id'])) {
+            $filter['_id'] = new \MongoDB\BSON\ObjectID($filter['id']);
+            unset($filter['id']);
+        }
+        $result = $collection->updateMany($filter, $values_set);
+
+        return $result->getModifiedCount();
+    }
+
+    public function delete($collection, $filter)
+    {
+        $collection = $this->db->selectCollection($collection);
+        $filter = self::buildFilter($filter)[0];
+        if (isset($filter['id'])) {
+            $filter['_id'] = new \MongoDB\BSON\ObjectID($filter['id']);
+            unset($filter['id']);
+        }
+        $result = $collection->deleteMany($filter);
+
+        return $result->getDeletedCount();
+    }
+
+    public function find($collection, $filters, $fields = null, $sort = null, $start = 0, $limit = 25, $debug = false)
+    {
+        $collection = $this->db->selectCollection($collection);
+        if (isset($filters['id'])) {
+            $filters['_id'] = new \MongoDB\BSON\ObjectID($filters['id']);
+            unset($filters['id']);
+        }
+        $query_filters = [];
+        if ($filters != null) {
+            $query_filters = ['$and' => self::buildFilter($filters)];
+        }
+        $count = $collection->count($query_filters);
+        if ($count > 0) {
+            $results = [];
+            $options = [
+                'limit' => (int) $limit,
+                'skip' => (int) $start,
+                'typeMap' => ['root' => 'array', 'document' => 'array'],
+            ];
+            if ($fields!==null) {
+                $projection = [];
+                foreach ($fields as $field) {
+                    if ($field=='id') {
+                        $field = '_id';
+                    }
+                    $projection[$field] = true;
+                }
+                $options['projection'] = $projection;
+            }
+            if ($sort!==null) {
+                foreach ($sort as $sort_key => $sort_dir) {
+                    $sort[$sort_key] = ($sort_dir=='desc') ? -1 : 1;
+                    if ($sort_key=='id') {
+                        $sort['_id'] = $sort[$sort_key];
+                        unset($sort['id']);
+                    }
+                }
+                $options['sort'] = $sort;
+            }
+            $cursor = $collection->find($query_filters, $options);
+            $iterator = new \IteratorIterator($cursor);
+            $iterator->rewind();
+            while ($doc = $iterator->current()) {
+                if (isset($doc['_id'])) {
+                    $doc['id'] = (string) $doc['_id'];
+                    unset($doc['_id']);
+                }
+                $results[] = $doc;
+                $iterator->next();
+            }
+            return ['total' => $count, 'data' => $results];
+        }
+        return ['total' => 0, 'data' => null];
+    }
+
+    public function query($query)
+    {
+        // reserved
+    }
+
+    public static function buildFilter($filter)
+    {
+        $filters = [];
+        foreach ($filter as $key => $value) {
+            if (strpos($key, '__')!==false) {
+                preg_match('/__(.*?)$/i', $key, $matches);
+                $operator = $matches[1];
+                switch ($operator) {
+                    case '!in':
+                        $operator = 'nin';
+                        break;
+                    case 'not':
+                        $operator = 'ne';
+                        break;
+                    case 'wildcard':
+                        $operator = 'regex';
+                        $value = str_replace(array('?'), array('.'), $value);
+                        break;
+                    case 'prefix':
+                        $operator = 'regex';
+                        $value = $value.'*';
+                        break;
+                }
+                $key = str_replace($matches[0], '', $key);
+                $filters[] = [$key => ['$'.$operator => $value]];
+            } elseif (strpos($key, '__')===false && is_array($value)) {
+                $filters[]['$or'] = self::buildFilter($value);
+            } else {
+                $filters[][$key] = $value;
+            }
+        }
+
+        return $filters;
+    }
+}
